058c3dfd5ec1a876594bd3b7632f046972c7ad71
ichiro.tanimoto <ec2-user@ip-172-31-39-134.us-east-2.compute.internal>
2018-06-26 03:52:01 +0000

    have

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -86,10 +86,12 @@ use App\Item;
     {
       $item = Item::find($id);
       $want_users = $item->want_users;
+      $have_users = $item->haveusers;
 
       return view('items.show', [
           'item' => $item,
           'want_users' => $want_users,
+          'have_users' => $have_users,
       ]);
     }
   }
